bba088aa4368f73ac4f7742e5c27bf033da0b950
monilouise <moniquelouise@gmail.com>
2025-07-11 23:14:36 -0300

    fix to display output in real time

--- codeflowlm/command.py
+++ codeflowlm/command.py
@@ -1,8 +1,20 @@
 import subprocess
 
+"""
 def execute_command(command):
     print(command)
     result = subprocess.run(command, shell=True, text=True)
     print(result.stdout)
     if result.stderr:
       print("Error:", result.stderr)
+"""
+   
+def execute_command(command):
+    print(command)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    # Mostra a saída em tempo real
+    for line in process.stdout:
+        print(line, end='')  # evita pular linhas duplas
+
+    process.wait()  # Aguarda o término
