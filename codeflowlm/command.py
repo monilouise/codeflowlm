db911f07863c7c0ecb1e81f16aee002fecbde82e
monilouise <moniquelouise@gmail.com>
2025-07-11 23:19:38 -0300

    refactoring

--- codeflowlm/command.py
+++ codeflowlm/command.py
@@ -1,21 +1,11 @@
 import subprocess
 
-"""
-def execute_command(command):
-    print(command)
-    result = subprocess.run(command, shell=True, text=True)
-    print(result.stdout)
-    if result.stderr:
-      print("Error:", result.stderr)
-"""
-   
 def execute_command(command):
     print(command)
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
-    # Lê e imprime cada linha assim que disponível
     for line in iter(process.stdout.readline, ''):
-        print(line, end='', flush=True)  # flush força o print imediato no Colab
+        print(line, end='', flush=True)  
 
     process.stdout.close()
     process.wait()
